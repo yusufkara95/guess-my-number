0b42bbbadca3ffde05efe5cbd9bc223b24d9806e
yusufkara <yusufkara@gmx.de>
2022-08-13 15:39:27 +0200

    Add the file

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+
+document.querySelector('.check').addEventListener
+('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Number';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number!';
+    }
+});
